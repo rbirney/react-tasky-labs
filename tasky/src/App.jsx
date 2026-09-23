34de063b6f4c96b062283bb9102814ee0a3d365d
rbirney <rbirney@wit.ie>
2026-09-23 02:05:52 +0100

    Delete a task

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -19,6 +19,12 @@ function App() {
     // console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
   
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
+  
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -30,6 +36,7 @@ function App() {
           done={task.done}
           key={task.id}
           markDone={() => doneHandler(index)}
+          deleteTask = {() => deleteHandler(index)}
         />
       ))}
     </div>
